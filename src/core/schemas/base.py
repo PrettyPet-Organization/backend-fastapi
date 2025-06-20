95ab85f8f678ee4060ea9062864a7e60d16a29b0
sanho50001 <sanho50001@bk.ru>
2025-06-20 15:43:18 +0300

    [fix/TASK-PPP-3]: Исправлена человеческая ошибка.

--- src/core/schemas/base.py
+++ src/core/schemas/base.py
@@ -3,5 +3,5 @@ from pydantic.alias_generators import to_camel
 
 
 class CamelCaseModel(BaseModel):
-    """Converter snake_case in camelCase"""
+    """Converter snake_case in camelCase."""
     model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
