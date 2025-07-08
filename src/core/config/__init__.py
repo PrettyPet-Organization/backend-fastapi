83605a465aae22c2569466e7ab7c26b81fb62691
Размик <chatikyanrazmik@gmail.com>
2025-07-08 22:10:12 +0300

    проверь core/config/__init__.py

--- src/core/config/__init__.py
+++ src/core/config/__init__.py
@@ -2,31 +2,26 @@ import os
 from collections.abc import AsyncGenerator
 
 import sqlalchemy
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 from str2bool import str2bool_exc
-
 from core.config.db import DatabaseSettings
 from core.config.logging import LOGGING_CONFIG, logger
 from core.config.main import BASE_DIR, Settings
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 DOTENV_MODE = str2bool_exc(os.getenv("DOTENV_MODE", "false"))
 
-if DOTENV_MODE:
-    from dotenv import load_dotenv
-
-    load_dotenv()
-    pg_internal_host = "localhost"
-else:
-    pg_internal_host = os.environ["PG_INTERNAL_HOST"]
-
+pg_internal_host = "localhost" if DOTENV_MODE else os.environ["PG_INTERNAL_HOST"]
 
 DEBUG = str2bool_exc(os.environ["DEBUG"])
-
 PG_DATABASE_NAME = os.environ["PG_DATABASE_NAME"]
 PG_USER_NAME = os.environ["PG_USER_NAME"]
 PG_USER_PASSWORD = os.environ["PG_USER_PASSWORD"]
 PG_INTERNAL_PORT = int(os.environ["PG_INTERNAL_PORT"])
+
 
 db_url = sqlalchemy.URL.create(
     drivername="postgresql+psycopg",
@@ -36,6 +31,7 @@ db_url = sqlalchemy.URL.create(
     port=PG_INTERNAL_PORT,
     database=PG_DATABASE_NAME,
 )
+
 
 db_settings = DatabaseSettings(url=db_url, echo=True)
 settings = Settings(debug=DEBUG, db=db_settings)
