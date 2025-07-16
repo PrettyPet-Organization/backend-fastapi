05d1f534fc6d6e79b04c0742383c032e67046b21
Размик <chatikyanrazmik@gmail.com>
2025-07-16 20:57:56 +0300

    unit тесты на ррр-2

--- src/core/schemas/user.py
+++ src/core/schemas/user.py
@@ -1,11 +1,11 @@
-from pydantic import ConfigDict, EmailStr
+from pydantic import ConfigDict, EmailStr, constr
 
 from core.schemas.base import CamelBaseModel
 
 
 class UserCreate(CamelBaseModel):
     email: EmailStr
-    password: str
+    password: constr(min_length=6)
 
 
 class UserRead(CamelBaseModel):
